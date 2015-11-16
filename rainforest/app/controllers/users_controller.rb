0065212ac6ed2e38b0e32f454ad2552c43c8d5fd
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-11-16 10:08:27 -0500

    Adding user names

--- rainforest/app/controllers/users_controller.rb
+++ rainforest/app/controllers/users_controller.rb
@@ -4,16 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		redirect_to products_url, notice: "Signed up!"
-  	else
-  		render "new"
+	 @user = User.new(user_params)
+	 if @user.save
+	   redirect_to products_url, notice: "Signed up!"
+	 else
+	   render "new"
+	 end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end
 
-private
-def user_params
-	params.require(:user).permit(:email, :password, :password_confirmation)
-end
-end
+
--- rainforest/db/schema.rb
+++ rainforest/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113150921) do
+ActiveRecord::Schema.define(version: 20151116144538) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20151113150921) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
